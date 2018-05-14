1b28203beb1221a01f45e0fc53160c3a51d37fec
emilywhitaker15@gmail.com <emilywhitaker15@gmail.com>
2018-05-14 22:11:01 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 // write your code below!
 
 var name = "Emily"
-var height = "74"
+var height = 74
 
-console.log(parseInt(height) +1)
+console.log(height +1)
 
 console.log("Name:")
 console.log(name)
